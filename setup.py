80cb0dfc6cc08c2fcaf10ae3449df021b3c22ddd
Wolfgang Noichl <wolfgang.noichl@tum.de>
2017-05-17 18:58:56 +0200

    ENH: Converted into installable package

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+setup(name='roipoly',
+      authors="Joerg Doepfert, Daniel Kornhauser",
+      packages=['roipoly'],
+      url='https://github.com/jdoepfert/roipoly.py',
+      install_requires=['numpy', 'matplotlib'])
