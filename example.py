a5c85a60f8473223a4a51171bc75833080ecd926
Joerg <joerg.doepfert@flixbus.com>
2018-06-09 13:22:14 +0200

    code cleanup

--- example.py
+++ example.py
@@ -20,27 +20,27 @@ plt.colorbar()
 plt.title("left click: line segment         right click: close region")
 
 # let user draw first ROI
-ROI1 = Roipoly(roicolor='r') #let user draw first ROI
+roi1 = Roipoly(roicolor='r') #let user draw first ROI
 
 # show the image with the first ROI
 plt.imshow(img, interpolation='nearest', cmap="Greys")
 plt.colorbar()
-ROI1.display_roi()
+roi1.display_roi()
 plt.title('draw second ROI')
 
 # let user draw second ROI
-ROI2 = Roipoly(roicolor='b') #let user draw ROI
+roi2 = Roipoly(roicolor='b') #let user draw ROI
 
 # show the image with both ROIs and their mean values
 plt.imshow(img, interpolation='nearest', cmap="Greys")
 plt.colorbar()
-[x.display_roi() for x in [ROI1, ROI2]]
-[x.display_mean(img) for x in [ROI1, ROI2]]
+[x.display_roi() for x in [roi1, roi2]]
+[x.display_mean(img) for x in [roi1, roi2]]
 plt.title('The two ROIs')
 plt.show()
 
 # show ROI masks
-plt.imshow(ROI1.get_mask(img) + ROI2.get_mask(img),
+plt.imshow(roi1.get_mask(img) + roi2.get_mask(img),
           interpolation='nearest', cmap="Greys")
 plt.title('ROI masks of the two ROIs')
 plt.show()
