0a5bf8fab0570ae281483e902316ff2117c238c2
Joerg <joerg.doepfert@flixbus.com>
2019-01-05 12:44:13 +0100

    update example

--- example.py
+++ example.py
@@ -2,36 +2,40 @@ import logging
 
 import numpy as np
 from matplotlib import pyplot as plt
-from roipoly import Roipoly
 
+from roipoly import RoiPoly
 
 logger = logging.getLogger(__name__)
 
-logging.basicConfig(format='%(levelname)s ''%(processName)-10s : %(asctime)s %(module)'
-                            's.%(funcName)s:%(lineno)s %(message)s',
+logging.basicConfig(format='%(levelname)s ''%(processName)-10s : %(asctime)s '
+                           '%(module)s.%(funcName)s:%(lineno)s %(message)s',
                     level=logging.DEBUG)
 
-# create image
+# Create image
 img = np.ones((100, 100)) * range(0, 100)
 
-# show the image
+# Show the image
+fig = plt.figure()
 plt.imshow(img, interpolation='nearest', cmap="Greys")
 plt.colorbar()
 plt.title("left click: line segment         right click: close region")
+plt.show(block=False)
 
-# let user draw first ROI
-roi1 = Roipoly(roicolor='r') #let user draw first ROI
+# Let user draw first ROI
+roi1 = RoiPoly(color='r', fig=fig)
 
-# show the image with the first ROI
+# Show the image with the first ROI
+fig = plt.figure()
 plt.imshow(img, interpolation='nearest', cmap="Greys")
 plt.colorbar()
 roi1.display_roi()
 plt.title('draw second ROI')
+plt.show(block=False)
 
-# let user draw second ROI
-roi2 = Roipoly(roicolor='b') #let user draw ROI
+# Let user draw second ROI
+roi2 = RoiPoly(color='b', fig=fig)
 
-# show the image with both ROIs and their mean values
+# Show the image with both ROIs and their mean values
 plt.imshow(img, interpolation='nearest', cmap="Greys")
 plt.colorbar()
 [x.display_roi() for x in [roi1, roi2]]
@@ -39,10 +43,8 @@ plt.colorbar()
 plt.title('The two ROIs')
 plt.show()
 
-# show ROI masks
+# Show ROI masks
 plt.imshow(roi1.get_mask(img) + roi2.get_mask(img),
-          interpolation='nearest', cmap="Greys")
+           interpolation='nearest', cmap="Greys")
 plt.title('ROI masks of the two ROIs')
 plt.show()
-
-
